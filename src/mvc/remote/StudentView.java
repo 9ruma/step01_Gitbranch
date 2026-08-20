4b20f5844b30922ea03a74bb958c8e7141d023f6
Joonhyeok Cha <chaa98@naver.com>
2026-08-20 11:14:35 +0900

    Update StudentView.java

--- src/mvc/remote/StudentView.java
+++ src/mvc/remote/StudentView.java
@@ -3,5 +3,7 @@ package mvc.remote;
 public class StudentView {
 	public void viewRemote() {
 		System.out.println("remote 수정");
+		System.out.println("remote 수정");
+		System.out.println("remote 수정");
 	}
 }
