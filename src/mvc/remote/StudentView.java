62626f112047aa868cc1f25ae7396318842526ab
9ruma <chaa98@naver.com>
2026-08-20 11:22:07 +0900

    로컬 수정

--- src/mvc/remote/StudentView.java
+++ src/mvc/remote/StudentView.java
@@ -3,7 +3,5 @@ package mvc.remote;
 public class StudentView {
 	public void viewRemote() {
 		System.out.println("remote 수정");
-		System.out.println("remote 수정");
-		System.out.println("remote 수정");
 	}
 }
